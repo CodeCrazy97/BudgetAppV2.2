e51cddc6b99f03de25fc2aae9973e8b70a1b80fd
CodeCrazy97 <evaughan72@yahoo.com>
2024-11-22 15:16:56 -0500

    Got budget report page working

--- BudgetApp_V2/BudgetApp_V2/ReportForm.cs
+++ BudgetApp_V2/BudgetApp_V2/ReportForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 
@@ -11,15 +12,16 @@ namespace BudgetApp_V2
     {
         public LinkedList<string> categories = new LinkedList<string>();  //Holds all categories.
         public LinkedList<double> totals = new LinkedList<double>();   //Holds the total amount spent for each category.
+        public SQLite sqlite = null;
 
         public ReportForm()
         {
+            this.sqlite = new SQLite();
             InitializeComponent();
         }
 
         private void ReportForm_Load(object sender, EventArgs e)
         {
-            DateTime dtSelected = Convert.ToDateTime(monthCalendar1.SelectionRange.Start.ToString());
             WindowState = FormWindowState.Maximized;
             //Select the first day of the first month of the current year for calendar 1.
             monthCalendar1.SetDate(Convert.ToDateTime(1 + "/" + 1 + "/" + DateTime.Today.Year));
@@ -27,7 +29,6 @@ namespace BudgetApp_V2
             // Display the dates as text.
             date1Label.Text = "1/1/" + DateTime.Today.Year;
             date2Label.Text = DateTime.Today.Month + "/" + DateTime.Today.Day + "/" + DateTime.Today.Year;
-
         }        
 
         private void exitButton_Click(object sender, EventArgs e)
@@ -38,7 +39,7 @@ namespace BudgetApp_V2
         private void DisplayTransactionsSummary()
         {
             categories.Clear();
-            categories = new SQLite().GetCategories();
+            categories = this.sqlite.GetCategories();
             totals.Clear();
             dataGridView1.Rows.Clear();
 
@@ -47,8 +48,10 @@ namespace BudgetApp_V2
             //Get the selected dates and place them in proper MySQL format.
             DateTime dt1 = Convert.ToDateTime(monthCalendar1.SelectionRange.Start.ToString());
             DateTime dt2 = Convert.ToDateTime(monthCalendar2.SelectionRange.Start.ToString());
-            string date1 = dt1.Year + "-" + dt1.Month + "-" + dt1.Day;
-            string date2 = dt2.Year + "-" + dt2.Month + "-" + dt2.Day;
+
+            // Use the "yyyy-MM-dd" format to ensure zero-padding for month and day
+            string date1 = dt1.ToString("yyyy-MM-dd");
+            string date2 = dt2.ToString("yyyy-MM-dd");
 
             // Display the date as text.
             date1Label.Text = dt1.Month + "/" + dt1.Day + "/" + dt1.Year;
@@ -58,44 +61,41 @@ namespace BudgetApp_V2
             {
                 warningLabel.Visible = false;
 
-                string connStr = new MySQLConnection().connection;
-                MySqlConnection conn = new MySqlConnection(connStr);
-                try
+                for (int i = 0; i < categories.Count; i++)  //Loop through all categories, getting totals for each.
                 {
-                    conn.Open();
-                    for (int i = 0; i < categories.Count; i++)  //Loop through all categories, getting totals for each.
+
+                    using (var selectCommand = this.sqlite.connection_object.CreateCommand())
                     {
-                        string sql = "SELECT SUM(amount) FROM expenses WHERE expense_type = '" + categories.ElementAt(i) + "' AND trans_date BETWEEN '" + date1 + "' AND '" + date2 + "'; ";
                         if (categories.ElementAt(i).ToLower() == "tithe")
                         {
                             // Negative amount for tithe means it was an expense (a positive amount means it's a tithe that is earmarked)
-                            sql = "SELECT SUM(amount) FROM expenses WHERE expense_type = '" + categories.ElementAt(i) + "' AND amount < 0 AND trans_date BETWEEN '" + date1 + "' AND '" + date2 + "'; "; ;
-                        }
-
-                        MySqlCommand cmd = new MySqlCommand(sql, conn);
-                        var reader = cmd.ExecuteReader();             //execute the command
-                        while (reader.Read())
+                            selectCommand.CommandText = @"SELECT SUM(amount) FROM expenses WHERE expense_type = @expense_type AND amount < 0 AND trans_date BETWEEN @date1 AND @date2; ";
+                        } else
                         {
-                            if (reader.IsDBNull(0))
-                            {
-                                totals.AddLast(0);
-                            }
-                            else
-                            {
-                                totals.AddLast(Math.Abs(reader.GetDouble(0)));
-                            }
+                            selectCommand.CommandText = @"SELECT SUM(amount) FROM expenses WHERE expense_type = @expense_type AND trans_date BETWEEN @date1 AND @date2; ";
                         }
-                        reader.Close();
+                        selectCommand.Parameters.AddWithValue("@expense_type", categories.ElementAt(i).ToLower());
+                        selectCommand.Parameters.AddWithValue("@date1", date1);
+                        selectCommand.Parameters.AddWithValue("@date2", date2);
+
+                        using (var reader = selectCommand.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                if (reader.IsDBNull(0))
+                                {
+                                    totals.AddLast(0);
+                                }
+                                else
+                                {
+                                    totals.AddLast(Math.Abs(reader.GetDouble(0)));
+                                }
+                            }
+                            reader.Close();
+                        }
                     }
                 }
-                
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.ToString());
-                }
-                conn.Close();
-                                
-                
+
                 //Get the total amount spent.
                 double total = 0.0;
                 for (int i = 0; i < totals.Count; i++)
@@ -141,13 +141,8 @@ namespace BudgetApp_V2
                 dataGridView1.ScrollBars = ScrollBars.None;
             }
 
-
-
-
-
             //Sort the transactions according to amount, in descending order.
             this.dataGridView1.Sort(dataGridView1.Columns[1], ListSortDirection.Descending);
-
         }
 
         private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
